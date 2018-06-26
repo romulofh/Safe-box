245e104b96e4782b86cb41f80158479a7f47d174
Romulo <romulofurtadoo@gmail.com>
2018-06-25 22:54:26 -0300

    Add nome ao User

---
+++ db/migrate/20180625233741_change_column_name.rb
@@ -0,0 +1,5 @@
+class ChangeColumnName < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :accounts, :type, :account_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620220958) do
+ActiveRecord::Schema.define(version: 20180625233741) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "number"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "agency"
-    t.string   "type"
+    t.string   "account_type"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20180620220958) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
